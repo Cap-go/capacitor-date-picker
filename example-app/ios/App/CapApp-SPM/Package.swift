b05896215ff5fecc774a3e46f8ad850ac50e32bf
Martin Donadieu <martindonadieu@gmail.com>
2026-05-28 17:29:05 +0200

    feat: add native example app shells

---
+++ example-app/ios/App/CapApp-SPM/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+// DO NOT MODIFY THIS FILE - managed by Capacitor CLI commands
+let package = Package(
+    name: "CapApp-SPM",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "CapApp-SPM",
+            targets: ["CapApp-SPM"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.3.4"),
+        .package(name: "CapgoCapacitorDatePicker", path: "../../../node_modules/.bun/@capgo+capacitor-date-picker@file+../node_modules/@capgo/capacitor-date-picker"),
+        .package(name: "CapgoCapacitorUpdater", path: "../../../node_modules/.bun/@capgo+capacitor-updater@8.47.4+33da1c2fb16abc29/node_modules/@capgo/capacitor-updater")
+    ],
+    targets: [
+        .target(
+            name: "CapApp-SPM",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "CapgoCapacitorDatePicker", package: "CapgoCapacitorDatePicker"),
+                .product(name: "CapgoCapacitorUpdater", package: "CapgoCapacitorUpdater")
+            ]
+        )
+    ]
+)
